10da23a97d298ebbe7cfad7fbfea451a842d1d1a
Gabriel Palmieri <gabriel.m.palmieri40@gmail.com>
2024-07-16 11:49:12 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,317 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import styled, { keyframes } from "styled-components";
+import Logo from "@/components/Logo";
+import { useEffect, useState } from "react";
+import { differenceInMilliseconds } from "date-fns";
+import { listCodes } from "@/components/list";
+
+const fadeIn = keyframes`
+  0%{
+    opacity: 0;
+  }
+  50%{
+    opacity: 1;
+  }
+  100%{
+    opacity: 0;
+  }
+`
+
+const fadeInStatic = keyframes`
+  0%{
+    opacity: 0;
+  }
+  100%{
+    opacity: 1;
+  }
+`
+
+const LoadingContainer = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20%;
+  flex-direction: column;
+
+  animation: ${fadeIn} 1.75s ease-in-out;
+`
+
+const LogoContainer = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  margin-top: 1%;
+  margin-left: 1%;
+  flex-direction: row;
+  animation: ${fadeInStatic} 1.75s ease-in-out;
+`
+
+const Title = styled.text`
+  color: black;
+  font-size: 60px;
+  font-family: inherit;
+`
+
+const SubTitle = styled.text`
+  color: black;
+  font-size: 24px;
+  font-family: inherit;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5%;
+  flex-direction: column;
+  animation: ${fadeInStatic} 2s ease-in-out;
+`
+
+const Text = styled.text`
+  color: #000;
+  font-family: inherit;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+` 
+
+const Input = styled.input<{valid: boolean}>`
+  width: 400px;
+  font-size: 100px;
+  text-align: center;
+  margin-top: 20px;
+  border: none;
+  border-bottom: 1px solid black;
+  border-bottom: 1px solid ${(props) => props.valid ? '#000' : '#A85100'};
+  color: ${(props) => props.valid ? '#000' : '#A85100'};
+  margin-top: 5%;
+
+  transition: all 1s ease-in-out;
+`
+
+const ContainerNumber = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5%;
+  flex-direction: column;
+  animation: ${fadeInStatic} 2s ease-in-out;
+`
+
+const Button = styled.button<{color: string, bgColor: string, hoverColor: string, hoverBg: string}>`
+  width: 280px;
+  height: 109px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: ${(props) => props.bgColor};
+
+  color: ${(props) => props.color};
+  font-family: inherit;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  
+  margin-top: 10%;
+  cursor: pointer;
+
+  transition: all 500ms ease;
+
+  &:hover {
+    background: ${(props) => props.hoverBg};
+    color: ${(props) => props.hoverColor};
+  }
+`
+
+const About = styled.div`
+  margin-left: 100px;
+
+  h1 {
+    color: #000;
+    font-family: inherit;
+    font-size: 84px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    text-transform: uppercase;
+  }
+
+  h2 {
+    color: var(--error-color);
+    font-family: inherit;
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    margin-top: -60px;
+  }
+
+  h3 {
+    color: #000;
+    font-family: inherit;
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    margin-top: 50px;
+    margin-bottom: 42px;
+  }
+
+  text {
+
+    color: #000;
+    font-family: inherit;
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    margin-top: 10px;
+    display: flex;
+    flex: 1;
+    align-items: baseline;
+
+    span {
+      color: var(--error-color);
+      font-size: 20px;
+      margin-left: 10px;
+    }
+  }
+`
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  max-height: 200px;
+  flex: 1;
+  justify-content: flex-end;
+  margin-right: 20px;
+  gap: 20px;
+  border: none;
+  margin-top: -8%;
+`
 
 export default function Home() {
+
+  const [inputValue, setInputValue] = useState('')
+  const [inputError, setInputError] = useState(true)
+  const [getInfo, setInfo] = useState<any>({})
+
+  const [state, setState] = useState<'loading' | 'home' | 'painel' | ''>('')
+
+  const verificaCodigo = (codigoDesejado: string) => {
+    let codigoExiste = false;
+  
+    listCodes.forEach((item) => {
+      if (item.code === codigoDesejado) {
+        codigoExiste = true;
+        setInfo(item)
+      }
+    });
+  
+    return codigoExiste;
+  };
+
+  function handleConfirm() {
+
+    if (!verificaCodigo(inputValue)) {
+      setInputError(false)
+
+      setTimeout(() => {
+        setInputError(true)
+      }, 1000);
+
+      return;
+    }
+    setState('painel')
+    
+  }
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const sanitizedValue = value.replace(/\D/g, '');
+
+    let formattedValue = '';
+    for (let i = 0; i < sanitizedValue.length; i++) {
+      if (i === 3) {
+        formattedValue += '-';
+      }
+      formattedValue += sanitizedValue[i];
+    }
+  
+    setInputValue(formattedValue);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setState('loading')
+    }, 500);
+  }, [])
+
+  useEffect(() => {
+    setTimeout(() => {
+      setState('home')
+    }, 2000);
+  }, [])
+
+  if (state === '') {
+    return;
+  }
+
+  if (state === 'loading') {
+    return (
+    <LoadingContainer>
+      <Logo/>
+      <Title>EVO</Title>
+      <SubTitle>Conecte-se, Reúna-se</SubTitle>
+    </LoadingContainer>
+    )
+  }
+
+  if (state === 'painel') {
+
+    return(
+      <>
+        <LogoContainer>
+          <Logo/>
+          <Title>EVO</Title>
+        </LogoContainer>
+        <About>
+          <h3>Reunião de:</h3>
+          <h1>{getInfo.author}</h1>
+          <h2>{getInfo.time}</h2>
+          <h3>Presenças:</h3>
+
+          {getInfo.presencas.slice(0, 5).map((element: any) => {
+            return(
+              <text key={element.id}>{element.name}<span>{element.atArrived}</span></text>
+            )
+          })}
+
+          <text>+ {getInfo.presencas.length - 5} pessoas</text>
+        </About>
+      </>
+    )
+  }
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <LogoContainer>
+        <Logo/>
+        <Title>EVO</Title>
+      </LogoContainer>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Container>
+        <Text>Por favor inserir o codigo da reuniao abaixo:</Text>
+        <Input valid={inputError} placeholder="111-111" value={inputValue} onChange={onChangeHandler} maxLength={7}/>
+        <Button bgColor="black" color="white" hoverBg="white" hoverColor="black" onClick={() => handleConfirm()}>CONFIRMAR</Button>
+      </Container>
+    </>
   );
 }
---
+++ components/list.ts
@@ -0,0 +1,36 @@
+export const listCodes = [
+    {code: '111-111', author: 'Gabriel Palmieri', time: '19:00H ate 22:00H', presencas: [
+        { id: 11, name: "ANA CLARA FULANO", atArrived: "AS 19:01" },
+        { id: 10, name: "JOSE ANTONIO", atArrived: "AS 18:57" },
+        { id: 9, name: "CARLOS ANDRADE", atArrived: "AS 18:37" },
+        { id: 8, name: "MATHEUS ANT", atArrived: "AS 18:27" },
+        { id: 7, name: "PEDRO MILLES", atArrived: "AS 18:12" },
+        { id: 6, name: "Outra Pessoa", atArrived: "AS 18:00" }, // Exemplo adicional
+        { id: 5, name: "LUCAS SILVA", atArrived: "AS 19:15" },
+        { id: 4, name: "MARIA SOUZA", atArrived: "AS 19:30" },
+        { id: 3, name: "JULIA MARTINS", atArrived: "AS 19:45" },
+        { id: 2, name: "GABRIEL OLIVEIRA", atArrived: "AS 20:00" },
+        { id: 1, name: "FERNANDA COSTA", atArrived: "AS 20:15" },
+        { id: 0, name: "ANDRÉ SANTOS", atArrived: "AS 20:30" },
+    ]},
+
+    {
+        code: '222-222',
+        author: 'Isabela Carvalho',
+        time: '18:00H até 21:00H',
+        presencas: [
+          { id: 11, name: "ANA SOUSA", atArrived: "AS 18:55" },
+          { id: 10, name: "JOÃO SANTOS", atArrived: "AS 18:50" },
+          { id: 9, name: "MARCOS SILVA", atArrived: "AS 18:40" },
+          { id: 8, name: "LUCAS RIBEIRO", atArrived: "AS 18:30" },
+          { id: 7, name: "PEDRO MELO", atArrived: "AS 18:20" },
+          { id: 6, name: "CAROLINA MARTINS", atArrived: "AS 18:10" },
+          { id: 5, name: "FELIPE ALVES", atArrived: "AS 18:05" },
+          { id: 4, name: "MARIANA OLIVEIRA", atArrived: "AS 18:00" },
+          { id: 3, name: "GABRIELA COSTA", atArrived: "AS 19:15" },
+          { id: 2, name: "RAFAELA SANTOS", atArrived: "AS 19:30" },
+          { id: 1, name: "ANDRÉ ALMEIDA", atArrived: "AS 19:45" },
+          { id: 0, name: "GUSTAVO PEREIRA", atArrived: "AS 20:00" }
+        ]
+      }
+]
